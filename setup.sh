5712e1b0584b5167815cf9a01ee21c8946ded484
Roberto Carlos <roberto.reyes@taptap.pe>
2017-10-23 03:30:12 -0500

    version 1.0

---
+++ setup.sh
@@ -0,0 +1,7 @@
+PORT=8080
+sed -i "s/PORT/$PORT/g" conf/httpd.conf
+cp conf/httpd.conf /etc/httpd/conf/
+cp conf.modules.d/00-base.conf /etc/httpd/conf.modules.d/
+service httpd configtest
+echo "Must be restart the service"
+
